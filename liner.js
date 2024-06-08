4c81364931a05b71a16c0001fbe90ba7c3abf050
forampaneliya <159258864+forampaneliya@users.noreply.github.com>
2024-06-08 12:08:21 +0530

    Add files via upload

---
+++ liner.js
@@ -0,0 +1,17 @@
+let arr = [10, 20, 0, 40, 50, 60, 15, 8, 9];
+
+let ser = 50;
+let flag = true
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == ser) {
+        console.log("true");
+        return "true";
+    }
+
+    else {
+        console.log("false");
+
+    }
+
+}
